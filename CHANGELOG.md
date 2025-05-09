43b819075c0d439f5f7c384469a1bc054a4f92c7
Samantha Nguyen <contact@samanthanguyen.me>
2025-05-09 00:54:46 -0500

    feat: various updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,23 @@
 # Changelog
 
+## 1.1.0 (Unreleased)
+### Features
+- A new module under `./constants`, which exports constants of XML namespaces
+  ```diff
+  + export const namespaceHtml = 'http://www.w3.org/1999/xhtml'
+  + export const namespaceMathMl = 'http://www.w3.org/1998/Math/MathML'
+  + export const namespaceSvg = 'http://www.w3.org/2000/svg'
+  + export const namespaceXLink = 'http://www.w3.org/1999/xlink'
+  + export const namespaceXml = 'http://www.w3.org/XML/1998/namespace'
+  + export const namespaceXmlNs = 'http://www.w3.org/2000/xmlns/'
+  ```
+
+### Documentation
+- Improve JSR.io's rendering of `@see` annotations which references external links
+
+### Deprecated
+- `isAsciiWhitespace()` is deprecated. Use `isAsciiWsp()` instead.
+
 ## 1.0.1 (2025-05-02)
 - Fix minor rendering issue in README.md for jsr.io
 
---
+++ src/namespaces.ts
@@ -0,0 +1,29 @@
+/**
+ * @see {@link https://infra.spec.whatwg.org/#html-namespace}
+ */
+export const namespaceHtml = 'http://www.w3.org/1999/xhtml'
+
+/**
+ * @see {@link https://infra.spec.whatwg.org/#mathml-namespace}
+ */
+export const namespaceMathMl = 'http://www.w3.org/1998/Math/MathML'
+
+/**
+ * @see {@link https://infra.spec.whatwg.org/#svg-namespace}
+ */
+export const namespaceSvg = 'http://www.w3.org/2000/svg'
+
+/**
+ * @see {@link https://infra.spec.whatwg.org/#xlink-namespace}
+ */
+export const namespaceXLink = 'http://www.w3.org/1999/xlink'
+
+/**
+ * @see {@link https://infra.spec.whatwg.org/#xml-namespace}
+ */
+export const namespaceXml = 'http://www.w3.org/XML/1998/namespace'
+
+/**
+ * @see {@link https://infra.spec.whatwg.org/#xmlns-namespace}
+ */
+export const namespaceXmlNs = 'http://www.w3.org/2000/xmlns/'
