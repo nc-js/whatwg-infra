e04ad3133112906e9786475b51a77587f54a0290
Samantha <contact@samanthanguyen.me>
2025-05-09 01:07:31 -0500

    feat: add namespaces module (+ small fixes) (#108)

--- README.md
+++ README.md
@@ -4,9 +4,10 @@
 [![Codecov](https://img.shields.io/codecov/c/github/nc-js/whatwg-infra?style=flat-square&logo=codecov&logoColor=%23fff)](https://codecov.io/gh/nc-js/whatwg-infra)
 [![CI status](https://img.shields.io/github/actions/workflow/status/nc-js/whatwg-infra/.github%2Fworkflows%2Fdeno-ci.yml?style=flat-square)](https://github.com/nc-js/whatwg-infra/actions/workflows/deno-ci.yml)
 
-A small TypeScript package implementing various Unicode-related algorithms from the [**WHATWG Infra Standard**](https://infra.spec.whatwg.org/). This currently includes:
+A small TypeScript package implementing various algorithms from the [**WHATWG Infra Standard**](https://infra.spec.whatwg.org/). This currently includes:
  - [§ 4.5 Code points](https://infra.spec.whatwg.org/#code-points)
  - [§ 4.6 Strings](https://infra.spec.whatwg.org/#strings)
+ - [§ 8 Namespaces](https://infra.spec.whatwg.org/#namespaces)
 
 ## License
 
--- src/codePoints.ts
+++ src/codePoints.ts
@@ -22,7 +22,7 @@ export const isCodePointBetween = (
  * A code point that exists between 0 (`0x0000` NUL)
  * and 127 (`0x007F` DEL).
  *
- * @see https://infra.spec.whatwg.org/#ascii-byte
+ * @see {@link https://infra.spec.whatwg.org/#ascii-byte}
  */
 export const isAscii = (v: string): boolean => isCodePointBetween(v, 0x00, 0x7F)
 
@@ -31,7 +31,7 @@ export const isAscii = (v: string): boolean => isCodePointBetween(v, 0x00, 0x7F)
  *  - `U+D800` HIGH SURROGATES
  *  - `U+DFFF` LOW SURROGATES
  *
- * @see https://infra.spec.whatwg.org/#surrogate
+ * @see {@link https://infra.spec.whatwg.org/#surrogate}
  */
 export const isSurrogate = (v: string): boolean => {
 	for (const codePoint of v) {
@@ -46,8 +46,8 @@ export const isSurrogate = (v: string): boolean => {
 /**
  * A code point that is not a Unicode surrogate.
  *
- * @see https://infra.spec.whatwg.org/#surrogate
- * @see https://infra.spec.whatwg.org/#scalar-value
+ * @see {@link https://infra.spec.whatwg.org/#surrogate}
+ * @see {@link https://infra.spec.whatwg.org/#scalar-value}
  */
 export const isScalarValue = (v: string): boolean => !isSurrogate(v)
 
@@ -65,40 +65,14 @@ export const isScalarValue = (v: string): boolean => !isSurrogate(v)
  */
 export const isNonCharacter = (v: string): boolean => {
 	return isCodePointBetween(v, 0xFDD0, 0xFDEF) ||
+		// deno-fmt-ignore
 		[
-			0xFFFE,
-			0xFFFF,
-			0x1FFFE,
-			0x1FFFF,
-			0x2FFFE,
-			0x2FFFF,
-			0x3FFFE,
-			0x3FFFF,
-			0x4FFFE,
-			0x4FFFF,
-			0x5FFFE,
-			0x5FFFF,
-			0x6FFFE,
-			0x6FFFF,
-			0x7FFFE,
-			0x7FFFF,
-			0x8FFFE,
-			0x8FFFF,
-			0x9FFFE,
-			0x9FFFF,
-			0xAFFFE,
-			0xAFFFF,
-			0xBFFFE,
-			0xBFFFF,
-			0xCFFFE,
-			0xCFFFF,
-			0xDFFFE,
-			0xDFFFF,
-			0xEFFFE,
-			0xEFFFF,
-			0xFFFFE,
-			0xFFFFF,
-			0x10FFFE,
+			0xFFFE, 0xFFFF, 0x1FFFE, 0x1FFFF, 0x2FFFE, 0x2FFFF,
+			0x3FFFE, 0x3FFFF, 0x4FFFE, 0x4FFFF, 0x5FFFE, 0x5FFFF,
+			0x6FFFE, 0x6FFFF, 0x7FFFE, 0x7FFFF, 0x8FFFE, 0x8FFFF,
+			0x9FFFE, 0x9FFFF, 0xAFFFE, 0xAFFFF, 0xBFFFE, 0xBFFFF,
+			0xCFFFE, 0xCFFFF, 0xDFFFE, 0xDFFFF, 0xEFFFE, 0xEFFFF,
+			0xFFFFE, 0xFFFFF, 0x10FFFE,
 		].includes(v.codePointAt(0) as number)
 }
 
@@ -108,7 +82,7 @@ export const isNonCharacter = (v: string): boolean => {
  *  - `U+000A` EOL, LF, NL - End of line
  *  - `U+000D` CR - Carriage return
  *
- * @see https://infra.spec.whatwg.org/#ascii-tab-or-newline
+ * @see {@link https://infra.spec.whatwg.org/#ascii-tab-or-newline}
  */
 export const isAsciiTabOrNewline = (v: string): boolean =>
 	v === '\u{0009}' || v === '\u{000A}' || v === '\u{000D}'
@@ -117,18 +91,28 @@ export const isAsciiTabOrNewline = (v: string): boolean =>
  * A code point that's either an ASCII tab or newline,
  * `U+000C` FF Form Feed, or `U+0020` SP Space.
  *
- * @see https://infra.spec.whatwg.org/#ascii-tab-or-newline
- * @see https://infra.spec.whatwg.org/#ascii-whitespace
+ * @see {@link https://infra.spec.whatwg.org/#ascii-tab-or-newline}
+ * @see {@link https://infra.spec.whatwg.org/#ascii-whitespace}
  */
-export const isAsciiWhitespace = (v: string): boolean =>
+export const isAsciiWsp = (v: string): boolean =>
 	isAsciiTabOrNewline(v) || v === '\u{000C}' || v === '\u{0020}'
+
+/**
+ * A code point that's either an ASCII tab or newline,
+ * `U+000C` FF Form Feed, or `U+0020` SP Space.
+ *
+ * @see {@link https://infra.spec.whatwg.org/#ascii-tab-or-newline}
+ * @see {@link https://infra.spec.whatwg.org/#ascii-whitespace}
+ * @deprecated Use `isAsciiWsp()` instead.
+ */
+export const isAsciiWhitespace = (v: string): boolean => isAsciiWsp(v)
 
 /**
  * A code point inclusively between the range of:
  *  - `U+0000` NULL
  *  - `U+001F` INFORMATION SEPARATOR ONE
  *
- * @see https://infra.spec.whatwg.org/#c0-control
+ * @see {@link https://infra.spec.whatwg.org/#c0-control}
  */
 export const isC0Control = (v: string): boolean =>
 	isCodePointBetween(v, 0x0000, 0x001F)
@@ -136,8 +120,8 @@ export const isC0Control = (v: string): boolean =>
 /**
  * A code point that's either a C0 control or U+0020 SPACE.
  *
- * @see https://infra.spec.whatwg.org/#c0-control
- * @see https://infra.spec.whatwg.org/#c0-control-or-space
+ * @see {@link https://infra.spec.whatwg.org/#c0-control}
+ * @see {@link https://infra.spec.whatwg.org/#c0-control-or-space}
  */
 export const isC0ControlOrSpace = (v: string): boolean =>
 	isC0Control(v) || v === '\u{0020}'
@@ -148,8 +132,8 @@ export const isC0ControlOrSpace = (v: string): boolean =>
  * - `U+007F` DELETE
  * - `U+009F` APPLICATION PROGRAMMING COMMAND
  *
- * @see https://infra.spec.whatwg.org/#c0-control
- * @see https://infra.spec.whatwg.org/#control
+ * @see {@link https://infra.spec.whatwg.org/#c0-control}
+ * @see {@link https://infra.spec.whatwg.org/#control}
  */
 export const isControl = (v: string): boolean =>
 	isC0Control(v) || isCodePointBetween(v, 0x007F, 0x009F)
@@ -158,7 +142,7 @@ export const isControl = (v: string): boolean =>
  * A code point that is inclusively between the range of
  * `U+0030` (0) and `U+0039` (9).
  *
- * @see https://infra.spec.whatwg.org/#ascii-digit
+ * @see {@link https://infra.spec.whatwg.org/#ascii-digit}
  */
 export const isAsciiDigit = (v: string): boolean =>
 	isCodePointBetween(v, 0x0030, 0x0039)
@@ -167,7 +151,7 @@ export const isAsciiDigit = (v: string): boolean =>
  * A code point that is inclusively between the range of
  * `U+0041` (A) and `U+0046` (F).
  *
- * @see https://infra.spec.whatwg.org/#ascii-upper-hex-digit
+ * @see {@link https://infra.spec.whatwg.org/#ascii-upper-hex-digit}
  */
 export const isAsciiUpperHexDigit = (v: string): boolean =>
 	isCodePointBetween(v, 0x0041, 0x0046)
@@ -175,7 +159,7 @@ export const isAsciiUpperHexDigit = (v: string): boolean =>
 /**
  * A code point that is inclusively between the range of
  * `U+0061` (a) and `U+0066` (f).
- * @see https://infra.spec.whatwg.org/#ascii-lower-hex-digit
+ * @see {@link https://infra.spec.whatwg.org/#ascii-lower-hex-digit}
  */
 export const isAsciiLowerHexDigit = (v: string): boolean =>
 	isCodePointBetween(v, 0x0061, 0x0066)
@@ -183,9 +167,9 @@ export const isAsciiLowerHexDigit = (v: string): boolean =>
 /**
  * A code point that is either an ASCII lower or upper hex digit.
  *
- * @see https://infra.spec.whatwg.org/#ascii-upper-hex-digit
- * @see https://infra.spec.whatwg.org/#ascii-lower-hex-digit
- * @see https://infra.spec.whatwg.org/#ascii-hex-digit
+ * @see {@link https://infra.spec.whatwg.org/#ascii-upper-hex-digit}
+ * @see {@link https://infra.spec.whatwg.org/#ascii-lower-hex-digit}
+ * @see {@link https://infra.spec.whatwg.org/#ascii-hex-digit}
  */
 export const isAsciiHexDigit = (v: string): boolean =>
 	isAsciiLowerHexDigit(v) || isAsciiUpperHexDigit(v)
@@ -194,7 +178,7 @@ export const isAsciiHexDigit = (v: string): boolean =>
  * A code point that is inclusively between the range of
  * `U+0041` (A) and `U+005A` (Z).
  *
- * @see https://infra.spec.whatwg.org/#ascii-upper-alpha
+ * @see {@link https://infra.spec.whatwg.org/#ascii-upper-alpha}
  */
 export const isAsciiUpperAlpha = (v: string): boolean =>
 	isCodePointBetween(v, 0x0041, 0x005A)
@@ -203,7 +187,7 @@ export const isAsciiUpperAlpha = (v: string): boolean =>
  * A code point that is inclusively between the range of
  * `U+0061` (a) and `U+007A` (z).
  *
- * @see https://infra.spec.whatwg.org/#ascii-lower-alpha
+ * @see {@link https://infra.spec.whatwg.org/#ascii-lower-alpha}
  */
 export const isAsciiLowerAlpha = (v: string): boolean =>
 	isCodePointBetween(v, 0x0061, 0x007A)
@@ -212,9 +196,9 @@ export const isAsciiLowerAlpha = (v: string): boolean =>
  * A code point that is either an ASCII lower alpha or
  * ASCII upper alpha.
  *
- * @see https://infra.spec.whatwg.org/#ascii-lower-alpha
- * @see https://infra.spec.whatwg.org/#ascii-upper-alpha
- * @see https://infra.spec.whatwg.org/#ascii-alpha
+ * @see {@link https://infra.spec.whatwg.org/#ascii-lower-alpha}
+ * @see {@link https://infra.spec.whatwg.org/#ascii-upper-alpha}
+ * @see {@link https://infra.spec.whatwg.org/#ascii-alpha}
  */
 export const isAsciiAlpha = (v: string): boolean =>
 	isAsciiLowerAlpha(v) || isAsciiUpperAlpha(v)
@@ -222,8 +206,8 @@ export const isAsciiAlpha = (v: string): boolean =>
 /**
  * A code point that is an ASCII digit or ASCII alpha.
  *
- * @see https://infra.spec.whatwg.org/#ascii-numeric
- * @see https://infra.spec.whatwg.org/#ascii-alpha
+ * @see {{@link https://infra.spec.whatwg.org/#ascii-numeric}
+ * @see {{@link https://infra.spec.whatwg.org/#ascii-alpha}
  */
 export const isAsciiAlphanumeric = (v: string): boolean =>
 	isAsciiDigit(v) || isAsciiAlpha(v)
