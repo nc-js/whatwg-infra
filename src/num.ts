0196809e4fb9e110fa20558bbc56aff8fafdc7d3
Samantha Nguyen <contact@samanthanguyen.me>
2025-05-10 00:51:56 -0500

    add NaN in doctest/example + int128 tests

--- src/num.ts
+++ src/num.ts
@@ -6,7 +6,7 @@
 const checkInt = (n: number, min: number, max: number) =>
 	Number.isInteger(n) && n >= min && n <= max
 
-const bigCheck = (n: number | bigint, min: bigint, max: bigint) => {
+const checkBig = (n: number | bigint, min: bigint, max: bigint) => {
 	if (typeof n === 'number') {
 		if (Number.isInteger(n)) {
 			n = BigInt(n)
@@ -31,6 +31,7 @@ const bigCheck = (n: number | bigint, min: bigint, max: bigint) => {
  * assertFalse(isUint8(-1))
  * assertFalse(isUint8(256)) // 2^8
  * assertFalse(isUint8(0.5))
+ * assertFalse(isUint8(NaN))
  * ```
  */
 export const isUint8 = (n: number): boolean => checkInt(n, 0, 255)
@@ -48,7 +49,8 @@ export const isUint8 = (n: number): boolean => checkInt(n, 0, 255)
  * assert(isUint16(65535)) // (2^16)-1
  * assertFalse(isUint16(-1))
  * assertFalse(isUint16(65536)) // 2^16
- * assertFalse(isUint8(0.5))
+ * assertFalse(isUint16(0.5))
+ * assertFalse(isUint16(NaN))
  * ```
  */
 export const isUint16 = (n: number): boolean => checkInt(n, 0, 65535)
@@ -62,11 +64,12 @@ export const isUint16 = (n: number): boolean => checkInt(n, 0, 65535)
  * import { isUint32 } from '@nc/whatwg-infra/num'
  * import { assert, assertFalse } from '@std/assert'
  *
- * assert(isUint8(0))
+ * assert(isUint32(0))
  * assert(isUint32((2 ** 32) - 1))
  * assertFalse(isUint32(-1))
  * assertFalse(isUint32(2 ** 32))
  * assertFalse(isUint32(0.5))
+ * assertFalse(isUint32(NaN))
  * ```
  */
 export const isUint32 = (n: number): boolean => checkInt(n, 0, 4_294_967_295)
@@ -88,10 +91,11 @@ export const isUint32 = (n: number): boolean => checkInt(n, 0, 4_294_967_295)
  * assertFalse(isUint64(-1))
  * assertFalse(isUint64(2n ** 64n))
  * assertFalse(isUint64(0.5))
+ * assertFalse(isUint64(NaN))
  * ```
  */
 export const isUint64 = (n: number | bigint): boolean =>
-	bigCheck(n, 0n, 18_446_744_073_709_551_615n)
+	checkBig(n, 0n, 18_446_744_073_709_551_615n)
 
 /**
  * Checks if the number is a 128-bit unsigned integer.
@@ -109,10 +113,11 @@ export const isUint64 = (n: number | bigint): boolean =>
  * assert(isUint128((2n ** 128n) - 1n))
  * assertFalse(isUint128(-1))
  * assertFalse(isUint128(2n ** 128n))
+ * assertFalse(isUint128(NaN))
  * ```
  */
 export const isUint128 = (n: number | bigint): boolean =>
-	bigCheck(n, 0n, 340_282_366_920_938_463_463_374_607_431_768_211_455n)
+	checkBig(n, 0n, 340_282_366_920_938_463_463_374_607_431_768_211_455n)
 
 /**
  * Checks if the number is an 8-bit signed integer.
@@ -128,6 +133,7 @@ export const isUint128 = (n: number | bigint): boolean =>
  * assertFalse(isInt8(-129))
  * assertFalse(isInt8(128))
  * assertFalse(isInt8(0.5))
+ * assertFalse(isInt8(NaN))
  * ```
  */
 export const isInt8 = (n: number): boolean => checkInt(n, -128, 127)
@@ -146,6 +152,7 @@ export const isInt8 = (n: number): boolean => checkInt(n, -128, 127)
  * assertFalse(isInt16(-32769))
  * assertFalse(isInt16(32768))
  * assertFalse(isInt16(0.5))
+ * assertFalse(isInt16(NaN))
  * ```
  */
 export const isInt16 = (n: number): boolean => checkInt(n, -32768, 32767)
@@ -164,6 +171,7 @@ export const isInt16 = (n: number): boolean => checkInt(n, -32768, 32767)
  * assertFalse(isInt32(-2147483649))
  * assertFalse(isInt32(2147483648))
  * assertFalse(isInt32(0.5))
+ * assertFalse(isInt32(NaN))
  * ```
  */
 export const isInt32 = (n: number): boolean =>
@@ -186,13 +194,33 @@ export const isInt32 = (n: number): boolean =>
  * assertFalse(isInt64(-1n * 9223372036854775809n))
  * assertFalse(isInt64(9223372036854775808n))
  * assertFalse(isInt64(0.5))
+ * assertFalse(isInt64(NaN))
  * ```
  */
 export const isInt64 = (n: number | bigint): boolean =>
-	bigCheck(n, -1n * 9_223_372_036_854_775_808n, 9_223_372_036_854_775_807n)
+	checkBig(n, -1n * 9_223_372_036_854_775_808n, 9_223_372_036_854_775_807n)
 
+/**
+ * Checks if the number is a 128-bit signed integer.
+ *
+ * If the given argument is of type `number`, it will be internally
+ * changed to be a `bigint`.
+ *
+ * @example
+ * ```ts
+ * import { isInt128 } from '@nc/whatwg-infra/num'
+ * import { assert, assertFalse } from '@std/assert'
+ *
+ * assert(isInt128(-1n * 170_141_183_460_469_231_731_687_303_715_884_105_728n))
+ * assert(isInt128(170_141_183_460_469_231_731_687_303_715_884_105_727n))
+ * assertFalse(isInt128(-1n * 170_141_183_460_469_231_731_687_303_715_884_105_729n))
+ * assertFalse(isInt128(170_141_183_460_469_231_731_687_303_715_884_105_728n))
+ * assertFalse(isInt128(0.5))
+ * assertFalse(isInt128(NaN))
+ * ```
+ */
 export const isInt128 = (n: number | bigint): boolean =>
-	bigCheck(
+	checkBig(
 		n,
 		-1n * 170_141_183_460_469_231_731_687_303_715_884_105_728n,
 		170_141_183_460_469_231_731_687_303_715_884_105_727n,
